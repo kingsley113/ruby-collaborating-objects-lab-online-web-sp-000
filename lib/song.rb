f24150b3f3654e480ba86ac8d087a581ffa00426
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-08-12 09:41:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,7 @@ class Song
   def self.new_by_filename(filename)
     song = self.new
     # binding.pry
-    song.name = filename.split(" - ")[1]
+    @name = filename.split(" - ")[1]
     song
   end
 
