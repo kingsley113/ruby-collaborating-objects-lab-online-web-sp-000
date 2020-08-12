a5d4b8de2af72f9fd1b5a553d4fdf5af91e3b290
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-08-12 07:42:52 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -26,7 +26,7 @@ class Artist
     Song.all.select {|song| song.artist == self}
   end
 
-  def find_or_create_by_name(name)
+  def self.find_or_create_by_name(name)
     if (self.nil?)
       self.new(name)
     else
