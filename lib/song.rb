5b41bf9a5417649228a17bcb6f4b35e424be7894
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-08-12 09:58:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
 
   @@all = []
 
-  def initialize(name = "")
+  def initialize(name, artist = "")
     @name = name
     save
   end
@@ -24,11 +24,13 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    song = self.new
+    song_info = filename.split(" - ")
+
+    song = self.new(song_info[1], song_info[0])
     # binding.pry
-    song.name = filename.split(" - ")[1]
-    song.artist = filename.split(" - ")[0]
-    binding.pry
+    # song.name = filename.split(" - ")[1]
+    # song.artist = filename.split(" - ")[0]
+    # binding.pry
     song
   end
 
