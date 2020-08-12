e9e9d64fa84606847ce04be656568d932f663d4f
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-08-12 07:33:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,7 +18,13 @@ class Song
     @@all
   end
 
-
+  def artist_name=(name)
+    # if (self.artist.nil?)
+    #   self.artist = Artist.new(name)
+    # else
+    #   self.artist.name = name
+    # end
+  end
 
 
 
