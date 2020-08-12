333a1dfabb06536e95c000b641064cd39c22dfbd
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-08-12 10:29:33 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,27 +1,21 @@
 require 'pry'
 
 class MP3Importer
-
+  attr_accessor :files
   def initialize(file_path)
     files(file_path)
   end
 
-def files(path)
-  # binding.pry
-  files = Dir.entries("#{path}").select {|f| !File.directory? f}
-  binding.pry
-end
+  def files(path)
+    # binding.pry
+    @files = Dir.entries("#{path}").select {|f| !File.directory? f}
+    # binding.pry
+  end
 
 
-
-def import
-
-
-end
-
-
-
-
+  def import
+    @files.each {|file| Song.new_by_filename(file)}
+  end
 
 
 end
